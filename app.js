fd69734803916f18c076071762d81c31ca66f67d
Andrew Crow <andrew@crow.me>
2020-10-05 23:15:56 -0700

    formatted document and removed comments

--- app.js
+++ app.js
@@ -9,50 +9,17 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 // Required file imports
 const render = require("./lib/htmlRenderer");
 const employeeList = require("./lib/generateEmployeeList");
-const { checkForOutputDir } = require("./lib/checkForOutputDir");
-const { buildHtmlFile} = require("./lib/buildHtmlFile");
+const { checkForOutputDir } = require("./utils/checkForOutputDir");
+const { buildHtmlFile } = require("./utils/buildHtmlFile");
 
 // Function to initialize program
 async function init() {
     await employeeList.generateEmployeeList();
     const html = await render(employeeList.employees);
     await checkForOutputDir(OUTPUT_DIR);
-    buildHtmlFile(html,outputPath);
+    buildHtmlFile(html, outputPath);
 }
 
 // function call to initialize program
 init()
 
-// const buildHtmlFile = (html) => {
-//     fs.writeFile(outputPath,html, err => {
-//         if (err) {
-//             console.log(err);
-//         } else{
-//             console.log("Created team.html, success!");
-//         }
-//     })
-// }
-
-
-// const checkForOutputDir = () => {
-//     fs.access(OUTPUT_DIR, err => {
-//         if (err){
-//             console.log("Directory does not exist. Making a new directory.");
-//             makeOutputDir();
-//             return;
-//         } else {
-//             console.log("Directory found.");
-//             return;
-//         }
-//     })
-// }
-
-// const makeOutputDir = () => {
-//     fs.mkdir(OUTPUT_DIR, err => {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             console.log("Successfully created new directory.");
-//         }
-//     })
-// }
