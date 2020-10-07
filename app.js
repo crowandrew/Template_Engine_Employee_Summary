9fabda40bc8cfc9630df8e5f63f71e44fceca91e
Andrew Crow <andrew@crow.me>
2020-10-06 21:26:30 -0700

    moved build html to check for output dir

--- app.js
+++ app.js
@@ -8,15 +8,13 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 // Required file imports
 const render = require("./lib/htmlRenderer");
 const employeeList = require("./lib/generateEmployeeList");
-const { checkForOutputDir } = require("./lib/checkForOutputDir");
-const { buildHtmlFile } = require("./lib/buildHtmlFile");
+const { checkForOutputDir } = require("./lib/checkForOutputDir");;
 
 // Function to initialize program
 async function init() {
     await employeeList.generateEmployeeList();
     const html = await render(employeeList.employees);
-    await checkForOutputDir(OUTPUT_DIR);
-    buildHtmlFile(html, outputPath);
+    await checkForOutputDir(OUTPUT_DIR,html,outputPath);
 }
 
 // function call to initialize program
